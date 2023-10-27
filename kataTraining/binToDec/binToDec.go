7be103fde0023e8080183e2779aa7c8aff90ecd4
pdaambrosio <pda.ambrosio@gmail.com>
2023-10-26 21:28:08 -0300

    feat: binToDec

---
+++ kataTraining/binToDec/binToDec.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// The function takes a binary string as input and converts it to a decimal integer.
+func BinToDec(bin string) int {
+	if bin, err := strconv.ParseInt(bin, 2, 64); err == nil {
+		return int(bin)
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	fmt.Println(BinToDec("10"))
+	fmt.Println(BinToDec("11"))
+	fmt.Println(BinToDec("101010"))
+	fmt.Println(BinToDec("1001001"))
+}
