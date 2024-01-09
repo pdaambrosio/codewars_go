f467f9b9eaf169c8b1076db1a8d806d70e8aa6c3
pdaambrosio <pda.ambrosio@gmail.com>
2024-01-09 10:29:37 -0300

    feat: stringEndsWith

--- stringEndsWith/stringEndsWith.go
+++ stringEndsWith/stringEndsWith.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 )
 
-// The solution function checks if the ending of a given string matches a specified ending string.
+// The solution function checks if a given string ends with a specific ending string.
 func solution(str, ending string) bool {
+	if len(str) < len(ending) {
+		return false
+	}
+
 	return str[len(str)-len(ending):] == ending
 }
 
@@ -13,4 +17,19 @@ func main() {
 	fmt.Println(solution("abcde", "cde"))
 	fmt.Println(solution("abc", "c"))
 	fmt.Println(solution("a", "z"))
+	fmt.Println(solution("", "t"))
 }
+
+/*
+The best solutions in codewars:
+	package kata
+	import "strings"
+	func solution(str, ending string) bool {
+		return strings.HasSuffix(str, ending)
+	}
+and
+	package kata
+	func solution(str, ending string) bool {
+	return len(str) >= len(ending) && str[len(str) - len(ending):] == ending
+	}
+*/
