0d7e2773f4b824c58dfad9ad9f99f7006cdf1ded
pdaambrosio <pda.ambrosio@gmail.com>
2024-01-14 10:27:05 -0300

    feat: multiple3And5

---
+++ multiple3And5/go.mod
@@ -0,0 +1,3 @@
+module multiple3And5
+
+go 1.21.4
---
+++ multiple3And5/multiple3And5.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// The function calculates the sum of all numbers below a given number that are divisible by 3 or 5.
+func Multiple3And5(number int) int {
+	var sum int
+	for i := 1; i < number; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	
+	return sum
+}
+
+func main() {
+	fmt.Println(Multiple3And5(10))
+	fmt.Println(Multiple3And5(20))
+	fmt.Println(Multiple3And5(200))
+}
