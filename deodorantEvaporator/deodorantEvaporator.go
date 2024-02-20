540500773f366fd07d4feb85c1007604fce442a3
pdaambrosio <pda.ambrosio@gmail.com>
2024-02-20 09:02:19 -0300

    doc: added comments

--- deodorantEvaporator/deodorantEvaporator.go
+++ deodorantEvaporator/deodorantEvaporator.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// The Evaporator function calculates the number of days it takes for a liquid to evaporate below a
+// certain threshold based on the initial content and evaporation rate per day.
 func Evaporator(content float64, evapPerDay int, threshold int) int {
   var days int
   var thresholdContent float64 = content * float64(threshold) / 100
