8b3635c0f41d375e58f129e3b267d3f081d6b7a1
pdaambrosio <pda.ambrosio@gmail.com>
2024-03-11 08:02:50 -0300

    init buyingCar

---
+++ buyingCar/buyingCar.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func NbMonths(startPriceOld, startPriceNew, savingperMonth int, percentLossByMonth float64) [2]int {
+	// TODO: code
+}
+
+func main() {
+	fmt.Println(NbMonths(2000, 8000, 1000, 1.5)) // [6, 766]
+}
