1a14437e6d85685976349dc273ecb9610479b88d
pdaambrosio <pda.ambrosio@gmail.com>
2024-03-26 08:10:31 -0300

    feat: stopGninnipsMySdrow

--- stopGninnipsMySdrow/stopGninnipsMySdrow.go
+++ stopGninnipsMySdrow/stopGninnipsMySdrow.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// The SpinWords function takes a string, splits it into words, and reverses any word with a length of
+// 5 or more before joining the words back together.
 func SpinWords(str string) string {
 	words := strings.Split(str, " ")
 	for i, word := range words {
@@ -16,6 +18,8 @@ func SpinWords(str string) string {
 	return strings.Join(words, " ")
 }
 
+// The `reverse` function in Go takes a string as input and returns the string with its characters
+// reversed.
 func reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
