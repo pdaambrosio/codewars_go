ef5034e9f33a77238ddfb16487b4538ca0b03d41
pdaambrosio <pda.ambrosio@gmail.com>
2024-03-26 08:10:00 -0300

    feat: stopGninnipsMySdrow

--- stopGninnipsMySdrow/stopGninnipsMySdrow.go
+++ stopGninnipsMySdrow/stopGninnipsMySdrow.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func SpinWords(str string) string {
-// TODO: write your code here
+	words := strings.Split(str, " ")
+	for i, word := range words {
+		if len(word) >= 5 {
+			words[i] = reverse(word)
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
 
 func main() {
 	fmt.Println(SpinWords("Hey fellow warriors"))
