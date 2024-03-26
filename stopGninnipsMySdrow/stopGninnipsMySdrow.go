298d7468cc36ef9e6f052c61f93db2de37025c21
pdaambrosio <pda.ambrosio@gmail.com>
2024-03-26 07:59:54 -0300

    feat: stopGninnipsMySdrow

---
+++ stopGninnipsMySdrow/stopGninnipsMySdrow.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SpinWords(str string) string {
+// TODO: write your code here
+
+func main() {
+	fmt.Println(SpinWords("Hey fellow warriors"))
+	fmt.Println(SpinWords("This is a test"))
+	fmt.Println(SpinWords("This is another test"))
+}
