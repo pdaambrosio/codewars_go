57283cb8818fc3a0461b03c84972b77ab36a6a24
pdaambrosio <pda.ambrosio@gmail.com>
2024-03-29 11:31:14 -0300

    feat: unluckDays

--- unluckyDays/unluckyDays.go
+++ unluckyDays/unluckyDays.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+// The function `UnluckDays` calculates the number of Fridays that fall on the 13th day of each month
+// in a given year.
 func UnluckDays(year int) int {
-// TODO: return the number of Friday the 13ths in the given year
+	var unluckCount int = 0
+	for month := 1; month <= 12; month++ {
+		if time.Date(year, time.Month(month), 13, 0, 0, 0, 0, time.UTC).Weekday() == 5 {
+			unluckCount++
+		}
+	}
+
+	return unluckCount
 }
 
 func main() {
