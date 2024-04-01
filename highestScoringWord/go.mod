62882f5682ba574e04d117c76fcf7345e536ae56
pdaambrosio <pda.ambrosio@gmail.com>
2024-04-01 07:24:24 -0300

    feat: highestScoringWord

---
+++ highestScoringWord/go.mod
@@ -0,0 +1,3 @@
+module highestScoringWord.go
+
+go 1.21.8
---
+++ highestScoringWord/highestScoringWord.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func High(s string) string {
+	//TODO: Implement this function
+}
+
+func main() {
+	fmt.Println(High("abad"))
+	fmt.Println(High("taxi"))
+	fmt.Println(High("volcano"))
+}
