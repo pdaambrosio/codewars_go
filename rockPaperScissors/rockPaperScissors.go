45edc3f8f24bb53c12a97ef401cc19b12d55d488
pdaambrosio <pda.ambrosio@gmail.com>
2024-06-03 09:39:50 -0300

    fix: code smell string literals should not be duplicated

--- rockPaperScissors/rockPaperScissors.go
+++ rockPaperScissors/rockPaperScissors.go
@@ -11,22 +11,25 @@ func rockPaperScissors(p1, p2 string) string {
 		return "Draw!"
 	}
 
+	const player1 = "Player 1 won!"
+	const player2 = "Player 2 won!"
+
 	switch p1 {
 	case "rock":
 		if p2 == "scissors" {
-			return "Player 1 won!"
+			return player1
 		}
 	case "scissors":
 		if p2 == "paper" {
-			return "Player 1 won!"
+			return player1
 		}
 	case "paper":
 		if p2 == "rock" {
-			return "Player 1 won!"
+			return player1
 		}
 	}
 
-	return "Player 2 won!"
+	return player2
 }
 
 func main() {
